8555796ab95b9a9559416d213849eaa7f02a34a5
RobertGłowacki <glowackir26@gmail.com>
2025-03-21 11:15:25 +0100

    Add maven project.

--- src/main/java/KafkaProducerApp.java
+++ src/main/java/KafkaProducerApp.java
@@ -1,5 +1,3 @@
-package main.java;
-
 public class KafkaProducerApp {
 
     public static void main(String[] args) {
--- src/main/java/KafkaTopicProducer.java
+++ src/main/java/KafkaTopicProducer.java
@@ -1,5 +1,3 @@
-package main.java;
-
 public interface KafkaTopicProducer {
 
     void produce(String configType);
