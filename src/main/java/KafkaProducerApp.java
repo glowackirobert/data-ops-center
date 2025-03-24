cf2b6d94683f0c06f85c96840a021b4c1c3d5d77
RobertGłowacki <glowackir26@gmail.com>
2025-03-24 14:42:23 +0100

    Adjust creation of  KafkaCustomTopicProducer to changes.

--- src/main/java/KafkaProducerApp.java
+++ src/main/java/KafkaProducerApp.java
@@ -8,13 +8,13 @@ public class KafkaProducerApp {
     public static void main(String[] args) {
         LOG.info("Starting Kafka Producer Application");
 
-        String configType = "local"; // default to local
+        String configType = "local";
         if (args.length > 0) {
             configType = args[0];
         }
 
-        KafkaTopicProducer kafkaProducer = new KafkaCustomTopicProducer();
-        kafkaProducer.produce(configType);
+        KafkaTopicProducer kafkaProducer = new KafkaCustomTopicProducer(configType);
+        kafkaProducer.produce();
         LOG.info("Kafka Producer Application finished");
     }
 }
