eb0dfdfc1f2ecb5e4f7f253542e3a98703a0dcca
RobertGłowacki <glowackir26@gmail.com>
2025-03-24 19:30:45 +0100

    Add scripts for adding schema and table.

---
+++ scripts/topic_schema.sh
@@ -0,0 +1,18 @@
+{
+	"schemaName": "topic",
+	"dimensionFieldSpecs": [
+		{
+			"message": "name",
+			"dataType": "STRING"
+		}
+	],
+	"metricFieldSpecs": [],
+	"dateTimeFieldSpecs": [
+		{
+			"name": "eventTime",
+			"dataType": "LONG",
+			"format": "1:MILLISECONDS:EPOCH",
+			"granularity": "1:MILLISECONDS"
+		}
+	]
+}
