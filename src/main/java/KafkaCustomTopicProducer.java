9db67e4f693556fad4a831db782dfd33a779512f
RobertGłowacki <glowackir26@gmail.com>
2025-03-26 14:33:39 +0100

    Producing messages with avro object as value.

--- src/main/java/KafkaCustomTopicProducer.java
+++ src/main/java/KafkaCustomTopicProducer.java
@@ -1,17 +1,20 @@
+import avro.Trade;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import util.PropertiesLoader;
 
+import java.time.Instant;
 import java.util.Objects;
 import java.util.Properties;
 import java.util.stream.IntStream;
 
+import static util.PropertiesLoader.loadProperties;
+
 public class KafkaCustomTopicProducer implements KafkaTopicProducer {
 
     private static final Logger LOG = LoggerFactory.getLogger(KafkaCustomTopicProducer.class);
-    private final KafkaProducer<String, String> producer;
+    private final KafkaProducer<String, Trade> producer;
     private static final String TOPIC = "topic";
     private static final String PROPERTIES_FILE_TEMPLATE = "kafka-producer-%s.properties";
     private static final String JSON_MESSAGE_TEMPLATE = "{\"message\": \"%s_%d\"}";
@@ -19,18 +22,18 @@ public class KafkaCustomTopicProducer implements KafkaTopicProducer {
 
     public KafkaCustomTopicProducer(String configType) {
         String propertiesFile = String.format(PROPERTIES_FILE_TEMPLATE, configType);
-        Properties properties = PropertiesLoader.loadProperties(propertiesFile);
+        Properties properties = loadProperties(propertiesFile);
         this.producer = new KafkaProducer<>(Objects.requireNonNull(properties));
     }
 
     @Override
     public void produce() {
-        IntStream.range(0, numberOfMessages).forEachOrdered(i -> sendSingleMessage(this.producer, createJsonMessage(i)));
+        IntStream.range(0, numberOfMessages).forEachOrdered(i -> sendSingleMessage(this.producer, createAvroMessage(i)));
     }
 
-    private void sendSingleMessage(KafkaProducer<String, String> producer, String message) {
-        ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC, message);
-        LOG.info("Sending message to topic {}: {}", TOPIC, message);
+    private void sendSingleMessage(KafkaProducer<String, Trade> producer, Trade trade) {
+        ProducerRecord<String, Trade> record = new ProducerRecord<>(TOPIC, trade);
+        LOG.info("Sending message to topic {}: {}", TOPIC, trade);
 
         try {
             producer.send(record, this::handleSendResult).get();
@@ -43,6 +46,25 @@ public class KafkaCustomTopicProducer implements KafkaTopicProducer {
 
     private String createJsonMessage(int i) {
         return String.format(JSON_MESSAGE_TEMPLATE, TOPIC, i);
+    }
+
+    private Trade createAvroMessage(int i) {
+        return Trade.newBuilder()
+                .setId("1")
+                .setUUID("A")
+                .setTrade("B")
+                .setCurrency("B")
+                .setAmount("12")
+                .setT1("T1")
+                .setT2("T2")
+                .setT3("T3")
+                .setT4("T4")
+                .setT5("T5")
+                .setT6("T6")
+                .setT7("T7")
+                .setBValue(true)
+                .setCreatedAt(Instant.ofEpochSecond(System.currentTimeMillis()))
+                .build();
     }
 
     private void handleSendResult(org.apache.kafka.clients.producer.RecordMetadata metadata, Exception exception) {
