e1aac62cefa362bcfefeab61ad56a966efb47286
RobertGłowacki <glowackir26@gmail.com>
2025-07-30 19:33:43 +0200

    Refactor of kafka producer.

--- kafka-producer-app/src/main/java/KafkaCustomTopicProducer.java
+++ kafka-producer-app/src/main/java/KafkaCustomTopicProducer.java
@@ -6,9 +6,7 @@ import org.apache.kafka.clients.producer.RecordMetadata;
 
 import java.util.Objects;
 import java.util.Properties;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static util.PropertiesLoader.loadProperties;
@@ -16,14 +14,15 @@ import static util.PropertiesLoader.loadProperties;
 @Slf4j
 public class KafkaCustomTopicProducer implements KafkaTopicProducer, AutoCloseable {
 
-    private final KafkaProducer<String, Trade> producer;
-    private final ExecutorService executorService;
-
     private static final String TOPIC = "trade";
     private static final String PROPERTIES_FILE_TEMPLATE = "kafka-producer-%s.properties";
     private static final int NUMBER_OF_MESSAGES = 10_000_000;
     private static final int FLUSH_INTERVAL = 100_000;
     private static final int NUMBER_OF_THREADS = 4;
+
+    private final KafkaProducer<String, Trade> producer;
+    private final ExecutorService executorService;
+    private final AtomicInteger messageCounter = new AtomicInteger(0);
 
     public KafkaCustomTopicProducer(String configType) {
         Properties properties = loadProperties(String.format(PROPERTIES_FILE_TEMPLATE, configType));
@@ -33,28 +32,28 @@ public class KafkaCustomTopicProducer implements KafkaTopicProducer, AutoCloseab
 
     @Override
     public void produce() {
-        log.info("produce()");
-        AtomicInteger messageCounter = new AtomicInteger(0);
+        log.info("Starting to produce messages");
         for (int i = 0; i < NUMBER_OF_THREADS; i++) {
-            executorService.submit(() -> produceMessages(messageCounter));
+            executorService.submit(this::produceMessages);
         }
     }
 
     @Override
     public void close() {
-        log.info("close()");
-        shutdownAndAwaitTermination();
+        log.info("Closing producer and executor service");
+        shutdownExecutor();
         producer.close();
     }
 
-    private void produceMessages(AtomicInteger messageCounter) {
-        log.debug("produceMessages({})", messageCounter);
+    private void produceMessages() {
         int localCounter = 0;
         while (true) {
-            var currentMessage = messageCounter.getAndIncrement();
-            if (currentMessage >= NUMBER_OF_MESSAGES) break;
-
-            sendSingleMessage(createAvroMessage(currentMessage));
+            int currentMsgIndex = messageCounter.getAndIncrement();
+            if (currentMsgIndex >= NUMBER_OF_MESSAGES) {
+                break;
+            }
+            Trade trade = createAvroMessage(currentMsgIndex);
+            sendSingleMessage(trade);
 
             localCounter++;
             if (localCounter % FLUSH_INTERVAL == 0) {
@@ -65,41 +64,40 @@ public class KafkaCustomTopicProducer implements KafkaTopicProducer, AutoCloseab
     }
 
     private void sendSingleMessage(Trade trade) {
-        log.debug("sendSingleMessage({})", trade);
         ProducerRecord<String, Trade> record = new ProducerRecord<>(TOPIC, trade);
         producer.send(record, this::handleSendResult);
     }
 
-    private Trade createAvroMessage(int i) {
+    private Trade createAvroMessage(int index) {
         return Trade.newBuilder()
-                .setTradeId(String.valueOf(i))
+                .setTradeId(String.valueOf(index))
                 .setSymbol(RandomGenerator.generateStringValue())
                 .build();
     }
 
     private void handleSendResult(RecordMetadata metadata, Exception exception) {
         if (exception != null) {
-            log.error("Error sending message: {}", exception.getMessage());
+            log.error("Error sending message", exception);
         } else {
             log.debug("Message sent successfully: topic={}, partition={}, offset={}",
                     metadata.topic(), metadata.partition(), metadata.offset());
         }
     }
 
-    private void shutdownAndAwaitTermination() {
+    private void shutdownExecutor() {
         executorService.shutdown();
         try {
             if (!executorService.awaitTermination(1, TimeUnit.MINUTES)) {
+                log.warn("Executor did not terminate in the specified time, forcing shutdown");
                 executorService.shutdownNow();
                 if (!executorService.awaitTermination(1, TimeUnit.MINUTES)) {
-                    log.error("ExecutorService did not terminate");
+                    log.error("Executor failed to terminate");
                 }
             }
-        } catch (InterruptedException ex) {
-            log.error("Shutdown interrupted. Initiating forced shutdown.");
+        } catch (InterruptedException e) {
+            log.error("Interrupted while shutting down executor, forcing shutdown", e);
             executorService.shutdownNow();
             Thread.currentThread().interrupt();
         }
     }
-
 }
