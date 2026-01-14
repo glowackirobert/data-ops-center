481beed4238067bf8397f71a9e73f2d3fea62db5
RobertGłowacki <glowackir26@gmail.com>
2026-01-14 17:59:45 +0100

    Proper file names.

--- cluster-setup/table_config/add-tables.sh
+++ cluster-setup/table_config/add-tables.sh
@@ -17,15 +17,14 @@ curl -X POST -H "Content-Type: application/json" \
   -d @/opt/pinot/scripts/gdansk_public_transport_table_config.json \
   http://pinot-controller:9000/tables
 
-
 # Add trade schema
 curl -X POST -H "Content-Type: application/json" \
-  -d @/opt/pinot/scripts/trade_realtime_schema.json \
+  -d @/opt/pinot/scripts/trade_table_schema.json \
   http://pinot-controller:9000/schemas
 
 # Add trade table
 curl -X POST -H "Content-Type: application/json" \
-  -d @/opt/pinot/scripts/trade_realtime_config.json \
+  -d @/opt/pinot/scripts/trade_table_config.json \
   http://pinot-controller:9000/tables
 
 echo "Tables added successfully"
