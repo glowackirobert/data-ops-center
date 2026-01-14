53825c9e1d8f265eaca7c429f1461eccb7fb5276
RobertGłowacki <glowackir26@gmail.com>
2026-01-14 19:08:21 +0100

    Remove condition to wait for controller. Add values for server config file.

--- cluster-setup/table_config/add-tables.sh
+++ cluster-setup/table_config/add-tables.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -e
 
-# Wait for controller
-until curl -f http://pinot-controller:9000/health; do
-  echo "Waiting for controller..."
-  sleep 5
-done
-
 # Add gdansk public transport schema
 curl -X POST -H "Content-Type: application/json" \
   -d @/opt/pinot/scripts/gdansk_public_transport_table_schema.json \
