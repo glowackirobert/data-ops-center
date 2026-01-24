63dcfa80e5665f5032a020e1e4beaeb638f3b9e2
RobertGłowacki <glowackir26@gmail.com>
2026-01-24 21:06:29 +0100

    Resolves CRLF issues. Change kafka image.

--- cluster-setup/table_config/add-tables.sh
+++ cluster-setup/table_config/add-tables.sh
@@ -2,22 +2,26 @@
 set -e
 
 # Add gdansk public transport schema
-curl -X POST -H "Content-Type: application/json" \
+curl -X POST \
+  -H "Content-Type: application/json" \
   -d @/opt/pinot/scripts/gdansk_public_transport_table_schema.json \
   http://pinot-controller:9000/schemas
 
 # Add gdansk public transport table
-curl -X POST -H "Content-Type: application/json" \
+curl -X POST \
+  -H "Content-Type: application/json" \
   -d @/opt/pinot/scripts/gdansk_public_transport_table_config.json \
   http://pinot-controller:9000/tables
 
 # Add trade schema
-curl -X POST -H "Content-Type: application/json" \
+curl -X POST \
+  -H "Content-Type: application/json" \
   -d @/opt/pinot/scripts/trade_table_schema.json \
   http://pinot-controller:9000/schemas
 
 # Add trade table
-curl -X POST -H "Content-Type: application/json" \
+curl -X POST \
+  -H "Content-Type: application/json" \
   -d @/opt/pinot/scripts/trade_table_config.json \
   http://pinot-controller:9000/tables
 
