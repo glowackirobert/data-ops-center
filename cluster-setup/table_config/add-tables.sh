1e6afcf0e19871dd2eccdf037c1895cb9014c675
RobertGłowacki <glowackir26@gmail.com>
2026-01-28 16:58:43 +0100

    Update compose file to align currently used pinot version. Enhanced checks for deleting table and schema if exists.

--- cluster-setup/table_config/add-tables.sh
+++ cluster-setup/table_config/add-tables.sh
@@ -1,6 +1,50 @@
 #!/bin/bash
 set -e
 
+GDANSK_PUBLIC_TRANSPORT_TABLE="gdansk_public_transport_OFFLINE"
+URL="http://pinot-controller:9000/tables/$GDANSK_PUBLIC_TRANSPORT_TABLE"
+if [ "$(curl -s -o /dev/null -w "%{http_code}" $URL)" = "200" ]; then
+  echo "Deleting table: $GDANSK_PUBLIC_TRANSPORT_TABLE"
+  curl -X DELETE "$URL"
+else
+  echo "Table $GDANSK_PUBLIC_TRANSPORT_TABLE does not exist, skipping"
+fi
+
+
+GDANSK_PUBLIC_TRANSPORT_SCHEMA="gdansk_public_transport"
+URL="http://pinot-controller:9000/schemas/$GDANSK_PUBLIC_TRANSPORT_SCHEMA"
+
+STATUS=$(curl -s -o /dev/null -w "%{http_code}" "$URL")
+if [ "$STATUS" = "200" ]; then
+  echo "Deleting schema: $GDANSK_PUBLIC_TRANSPORT_SCHEMA"
+  curl -X DELETE "$URL"
+else
+  echo "Schema $GDANSK_PUBLIC_TRANSPORT_SCHEMA does not exist, skipping"
+fi
+
+
+TRADE_TABLE="trade_REALTIME"
+URL="http://pinot-controller:9000/tables/TRADE_TABLE"
+if [ "$(curl -s -o /dev/null -w "%{http_code}" $URL)" = "200" ]; then
+  echo "Deleting table: TRADE_TABLE"
+  curl -X DELETE "$URL"
+else
+  echo "Table $TRADE_TABLE does not exist, skipping"
+fi
+
+
+TRADE_SCHEMA="trade"
+URL="http://pinot-controller:9000/schemas/TRADE_SCHEMA"
+STATUS=$(curl -s -o /dev/null -w "%{http_code}" "$URL")
+
+if [ "$STATUS" = "200" ]; then
+  echo "Deleting schema: TRADE_SCHEMA"
+  curl -X DELETE "$URL"
+else
+  echo "Schema $TRADE_SCHEMA does not exist, skipping"
+fi
+
+
 # Add gdansk public transport schema
 curl -X POST \
   -H "Content-Type: application/json" \
