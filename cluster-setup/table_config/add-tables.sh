7cb5cbe756a6e204ba73ce74a7bc003cf96948ff
RobertGłowacki <glowackir26@gmail.com>
2026-06-19 08:27:17 +0200

    Fixed issues with script for adding tables in pinot - add flag to treat http errors as failures, adjust var.

--- cluster-setup/table_config/add-tables.sh
+++ cluster-setup/table_config/add-tables.sh
@@ -3,7 +3,7 @@ set -e
 
 GDANSK_PUBLIC_TRANSPORT_TABLE="gdansk_public_transport_OFFLINE"
 URL="http://pinot-controller:9000/tables/$GDANSK_PUBLIC_TRANSPORT_TABLE"
-if [ "$(curl -s -o /dev/null -w "%{http_code}" $URL)" = "200" ]; then
+if [ "$(curl -s -o /dev/null -w "%{http_code}" "$URL")" = "200" ]; then
   echo "Deleting table: $GDANSK_PUBLIC_TRANSPORT_TABLE"
   curl -X DELETE "$URL"
 else
@@ -14,8 +14,7 @@ fi
 GDANSK_PUBLIC_TRANSPORT_SCHEMA="gdansk_public_transport"
 URL="http://pinot-controller:9000/schemas/$GDANSK_PUBLIC_TRANSPORT_SCHEMA"
 
-STATUS=$(curl -s -o /dev/null -w "%{http_code}" "$URL")
-if [ "$STATUS" = "200" ]; then
+if [ "$(curl -s -o /dev/null -w "%{http_code}" "$URL")" = "200" ]; then
   echo "Deleting schema: $GDANSK_PUBLIC_TRANSPORT_SCHEMA"
   curl -X DELETE "$URL"
 else
@@ -24,9 +23,9 @@ fi
 
 
 TRADE_TABLE="trade_REALTIME"
-URL="http://pinot-controller:9000/tables/TRADE_TABLE"
-if [ "$(curl -s -o /dev/null -w "%{http_code}" $URL)" = "200" ]; then
-  echo "Deleting table: TRADE_TABLE"
+URL="http://pinot-controller:9000/tables/$TRADE_TABLE"
+if [ "$(curl -s -o /dev/null -w "%{http_code}" "$URL")" = "200" ]; then
+  echo "Deleting table: $TRADE_TABLE"
   curl -X DELETE "$URL"
 else
   echo "Table $TRADE_TABLE does not exist, skipping"
@@ -34,11 +33,9 @@ fi
 
 
 TRADE_SCHEMA="trade"
-URL="http://pinot-controller:9000/schemas/TRADE_SCHEMA"
-STATUS=$(curl -s -o /dev/null -w "%{http_code}" "$URL")
-
-if [ "$STATUS" = "200" ]; then
-  echo "Deleting schema: TRADE_SCHEMA"
+URL="http://pinot-controller:9000/schemas/$TRADE_SCHEMA"
+if [ "$(curl -s -o /dev/null -w "%{http_code}" "$URL")" = "200" ]; then
+  echo "Deleting schema: $TRADE_SCHEMA"
   curl -X DELETE "$URL"
 else
   echo "Schema $TRADE_SCHEMA does not exist, skipping"
@@ -46,25 +43,25 @@ fi
 
 
 # Add gdansk public transport schema
-curl -X POST \
+curl --fail -X POST \
   -H "Content-Type: application/json" \
   -d @/opt/pinot/scripts/gdansk_public_transport_table_schema.json \
   http://pinot-controller:9000/schemas
 
 # Add gdansk public transport table
-curl -X POST \
+curl --fail -X POST \
   -H "Content-Type: application/json" \
   -d @/opt/pinot/scripts/gdansk_public_transport_table_config.json \
   http://pinot-controller:9000/tables
 
 # Add trade schema
-curl -X POST \
+curl --fail -X POST \
   -H "Content-Type: application/json" \
   -d @/opt/pinot/scripts/trade_table_schema.json \
   http://pinot-controller:9000/schemas
 
 # Add trade table
-curl -X POST \
+curl --fail -X POST \
   -H "Content-Type: application/json" \
   -d @/opt/pinot/scripts/trade_table_config.json \
   http://pinot-controller:9000/tables
